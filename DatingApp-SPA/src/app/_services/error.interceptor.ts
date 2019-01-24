1affad9c03136edeccb8a98f93d03d68c69a3d3e
Joshua Smith <veryjoshua@gmail.com>
2019-01-24 09:32:22 -0500

    adding stuff

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -9,7 +9,6 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError(error => {
-        console.log(error);
         if (error instanceof HttpErrorResponse) {
           if (error.status === 401) {
             return throwError(error.statusText);
