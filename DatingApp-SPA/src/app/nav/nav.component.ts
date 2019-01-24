0385e8a439ee93899c6b31c300a55ecc1d8a07bb
Joshua Smith <veryjoshua@gmail.com>
2019-01-24 09:27:39 -0500

    End of section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
       .subscribe((next) => {
         console.log(`logged in successfully`);
       }, (error) => {
-        console.log(`Failed to login`);
+        console.log(error);
       });
   }
 
